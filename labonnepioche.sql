d5142dea8a68d8d96dcf459730942cc873877437
Alexandru <105431639+nitescuale@users.noreply.github.com>
2024-04-13 17:18:31 +0200

    Upload databse

---
+++ labonnepioche.sql
@@ -0,0 +1,202 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : sam. 13 avr. 2024 à 15:15
+-- Version du serveur : 8.2.0
+-- Version de PHP : 8.2.13
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `labonnepioche`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `annonces`
+--
+
+DROP TABLE IF EXISTS `annonces`;
+CREATE TABLE IF NOT EXISTS `annonces` (
+  `id_annonce` bigint NOT NULL,
+  `id_utilisateur` bigint NOT NULL,
+  `titre` text NOT NULL,
+  `description` text NOT NULL,
+  `prix` float NOT NULL,
+  `categorie` text NOT NULL,
+  `etat` text NOT NULL,
+  `date_publication` date NOT NULL,
+  PRIMARY KEY (`id_annonce`),
+  KEY `id_utilisateur` (`id_utilisateur`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `categories`
+--
+
+DROP TABLE IF EXISTS `categories`;
+CREATE TABLE IF NOT EXISTS `categories` (
+  `id_categorie` int NOT NULL,
+  `nom_categorie` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  PRIMARY KEY (`id_categorie`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `etats_produit`
+--
+
+DROP TABLE IF EXISTS `etats_produit`;
+CREATE TABLE IF NOT EXISTS `etats_produit` (
+  `id_etat` int NOT NULL,
+  `nom_etat` text NOT NULL,
+  PRIMARY KEY (`id_etat`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `favoris`
+--
+
+DROP TABLE IF EXISTS `favoris`;
+CREATE TABLE IF NOT EXISTS `favoris` (
+  `id_favori` int NOT NULL,
+  `id_annonce` bigint NOT NULL,
+  `id_utilisateur` bigint NOT NULL,
+  PRIMARY KEY (`id_favori`),
+  KEY `id_annonce` (`id_annonce`),
+  KEY `id_utilisateur` (`id_utilisateur`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `photos_annonces`
+--
+
+DROP TABLE IF EXISTS `photos_annonces`;
+CREATE TABLE IF NOT EXISTS `photos_annonces` (
+  `id_photo` bigint NOT NULL,
+  `id_annonce` bigint NOT NULL,
+  `url_photo` text NOT NULL,
+  PRIMARY KEY (`id_photo`),
+  KEY `id_annonce` (`id_annonce`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `reviews`
+--
+
+DROP TABLE IF EXISTS `reviews`;
+CREATE TABLE IF NOT EXISTS `reviews` (
+  `id_review` bigint NOT NULL,
+  `id_annonce` bigint NOT NULL,
+  `id_utilisateur` bigint NOT NULL,
+  `note` int NOT NULL,
+  `commentaire` text NOT NULL,
+  `date_review` date NOT NULL,
+  PRIMARY KEY (`id_review`),
+  KEY `id_annonce` (`id_annonce`),
+  KEY `id_utilisateur` (`id_utilisateur`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `transactions`
+--
+
+DROP TABLE IF EXISTS `transactions`;
+CREATE TABLE IF NOT EXISTS `transactions` (
+  `id_transaction` bigint NOT NULL,
+  `id_annonce` bigint NOT NULL,
+  `id_vendeur` bigint NOT NULL,
+  `id_acheteur` bigint NOT NULL,
+  `montant` float NOT NULL,
+  `date_transaction` date NOT NULL,
+  PRIMARY KEY (`id_transaction`),
+  KEY `id_annonce` (`id_annonce`),
+  KEY `id_vendeur` (`id_vendeur`),
+  KEY `id_acheteur` (`id_acheteur`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `utilisateurs`
+--
+
+DROP TABLE IF EXISTS `utilisateurs`;
+CREATE TABLE IF NOT EXISTS `utilisateurs` (
+  `id_utilisateur` bigint NOT NULL,
+  `nom` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `prenom` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `email` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `password` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `url_photo_profil` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `date_inscription` date NOT NULL,
+  `ville` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `pays` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `admin` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id_utilisateur`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `annonces`
+--
+ALTER TABLE `annonces`
+  ADD CONSTRAINT `annonces_ibfk_1` FOREIGN KEY (`id_utilisateur`) REFERENCES `utilisateurs` (`id_utilisateur`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Contraintes pour la table `favoris`
+--
+ALTER TABLE `favoris`
+  ADD CONSTRAINT `favoris_ibfk_1` FOREIGN KEY (`id_annonce`) REFERENCES `annonces` (`id_annonce`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `favoris_ibfk_2` FOREIGN KEY (`id_utilisateur`) REFERENCES `annonces` (`id_utilisateur`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Contraintes pour la table `photos_annonces`
+--
+ALTER TABLE `photos_annonces`
+  ADD CONSTRAINT `photos_annonces_ibfk_1` FOREIGN KEY (`id_annonce`) REFERENCES `annonces` (`id_annonce`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Contraintes pour la table `reviews`
+--
+ALTER TABLE `reviews`
+  ADD CONSTRAINT `reviews_ibfk_1` FOREIGN KEY (`id_annonce`) REFERENCES `annonces` (`id_annonce`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `reviews_ibfk_2` FOREIGN KEY (`id_utilisateur`) REFERENCES `utilisateurs` (`id_utilisateur`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Contraintes pour la table `transactions`
+--
+ALTER TABLE `transactions`
+  ADD CONSTRAINT `transactions_ibfk_1` FOREIGN KEY (`id_annonce`) REFERENCES `annonces` (`id_annonce`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `transactions_ibfk_2` FOREIGN KEY (`id_vendeur`) REFERENCES `annonces` (`id_utilisateur`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `transactions_ibfk_3` FOREIGN KEY (`id_acheteur`) REFERENCES `utilisateurs` (`id_utilisateur`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
