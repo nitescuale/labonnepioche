1d2ee9b00eac958f7244907bf8ae1fa30c24a228
Alexandru <nitescuale@cy-tech.fr>
2024-04-24 16:39:00 +0200

    maj

--- accueil.php
+++ accueil.php
@@ -14,8 +14,22 @@
         <link rel="stylesheet" type="text/css" href="accueil.css">
 
         <header> <!-- Entete -->
-            <a href="accueil.php"><img   src="logo_site.png" alt="Logo de mon site web" ></a> <!-- Ajout du logo, qui renvoie à l'accueil si cliqué -->         
-            <div class="search-bar">
+            <a href="accueil.php"><img   src="logo_site.png" alt="Logo de mon site web" ></a> <!-- Ajout du logo, qui renvoie à l'accueil si cliqué -->            
+            <?php
+            // 1. Récupérez les catégories depuis la base de données
+            $query = $bdd->query('SELECT nom_categorie FROM categories');
+
+            // 2. Parcourez les résultats et créez les éléments <li> correspondants
+            echo '<ul class="menu">';
+            while ($row = $query->fetch()) {
+                $nom_categorie = $row['nom_categorie'];
+                echo '<li>';
+                echo '<a href="accueil.php?categorie=' . $nom_categorie . '" id="' . $nom_categorie . '">' . $nom_categorie . '</a>';
+                echo '</li>';
+            }
+            echo '</ul>';
+            ?>
+        <div class="search-bar">
                 <input type="text" placeholder="Rechercher...">
                 <button type="submit">Rechercher</button>
             </div>
@@ -54,20 +68,46 @@
         </header>
 
         <?php
-        // 1. Récupérez les catégories depuis la base de données
-        $query = $bdd->query('SELECT nom_categorie FROM categories');
-
-        // 2. Parcourez les résultats et créez les éléments <li> correspondants
-        echo '<ul class="menu">';
-        while ($row = $query->fetch()) {
-            $nom_categorie = $row['nom_categorie'];
-            echo '<li>';
-            echo '<a href="accueil.php?categorie=' . $nom_categorie . '" id="' . $nom_categorie . '">' . $nom_categorie . '</a>';
-            echo '</li>';
+        if(isset($_GET['categorie']))
+        {
+            $categorie = htmlspecialchars($_GET['categorie']);
+                // Récupérez les annonces et les photos correspondantes filtrées sur la catégorie depuis la base de données
+                $query = $bdd->prepare('SELECT a.id_annonce, a.titre, a.prix, a.categorie, a.etat, p.url_photo 
+                FROM annonces AS a
+                LEFT JOIN photos_annonces AS p ON a.id_annonce = p.id_annonce
+                WHERE a.categorie = ?
+                GROUP BY a.id_annonce');
+                $query->execute(array($categorie));
+        }else{
+        // Récupérez les annonces et les photos correspondantes depuis la base de données
+        $query = $bdd->query('SELECT a.id_annonce, a.titre, a.prix, a.categorie, a.etat, p.url_photo 
+                            FROM annonces AS a
+                            LEFT JOIN photos_annonces AS p ON a.id_annonce = p.id_annonce
+                            GROUP BY a.id_annonce');
         }
-        echo '</ul>';
-        ?>
+        // Parcourez les résultats et affichez les annonces avec leurs photos
+        while ($row = $query->fetch()) {
+            $id_annonce = $row['id_annonce'];
+            $titre = $row['titre'];
+            $prix = $row['prix'];
+            $categorie = $row['categorie'];
+            $etat = $row['etat'];
+            $url_photo = $row['url_photo'];
 
+            echo '<div class="annonces-container">';
+                echo '<div class="annonces-wrapper">';
+                    echo '<div class="annonce">';
+                        echo '<img src="' . $url_photo . '" alt="' . $titre . '">';
+                        echo '<div class="details">';
+                        echo '<h2><a class="lien-annonce" href="annonce.php?annonce=' . $id_annonce . '">' . $titre . '</a></h2>';
+                        echo '<p class="etat">' . $etat . '</p>';
+                        echo '<p class="prix">' . $prix . ' € </p>';
+                        echo '</div>';
+                    echo '</div>';
+                echo '</div>';
+            echo '</div>';
+        }
+        ?>
     </body>
 
 </html>
