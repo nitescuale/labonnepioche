ea69a6b43948ee048bb3557a4acedd4169f2221d
Alexandru <nitescuale@cy-tech.fr>
2024-05-23 16:24:50 +0200

    maj

--- process_transaction.php
+++ process_transaction.php
@@ -1,6 +1,12 @@
 <?php
 session_start();
+
 require_once 'db_config.php';
+
+require 'vendor/autoload.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
 if (isset($_POST['id_annonce']) && isset($_SESSION['user'])) {
     $id_annonce = $_POST['id_annonce'];
@@ -42,18 +48,36 @@ if (isset($_POST['id_annonce']) && isset($_SESSION['user'])) {
     $insert_query = $bdd->prepare('INSERT INTO transactions (id_annonce, id_vendeur, id_acheteur, montant, date_transaction) VALUES (?, ?, ?, ?, ?)');
     if ($insert_query->execute(array($id_annonce, $id_vendeur, $id_acheteur, $montant, $date_transaction))) {
 
+
+
+        $mail = new PHPMailer(true);
+        // Paramètres SMTP
+        $mail->isSMTP();
+        $mail->Host = 'smtp.gmail.com'; // Adresse SMTP du serveur
+        $mail->SMTPAuth = true;
+        $mail->Username = 'nitescuale@cy-tech.fr'; // Adresse e-mail SMTP
+        $mail->Password = '-'; // Mot de passe SMTP, à modifier lors de l'utilisation
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        $mail->Port = 587;
+
         $headers = 'From: La Bonne Pioche' . "\r\n" .
                    'X-Mailer: PHP/' . phpversion();
 
         // Envoyer un email à l'acheteur
-        $sujet_acheteur = "Confirmation de votre achat";
-        $message_acheteur = "Bonjour,\n\nVous avez acheté l'annonce $titre_annonce pour un montant de $montant €. La transaction a été effectuée le $date_transaction.\n\nMerci pour votre achat. N'hésitez pas à prendre contact avec votre acheteur à l'adresse mail suivante : $email_vendeur pour organiser la livraison/récupération de votre bien";
-        mail($email_acheteur, $sujet_acheteur, $message_acheteur, $headers);
+        $mail->setFrom('nitescuale@cy-tech.fr', 'La Bonne Pioche');
+        $mail->addAddress($email_acheteur, 'Nom du destinataire');
+        $mail->isHTML(true);
+        $mail->Subject = 'Confirmation de votre achat';
+        $mail->Body = "Bonjour,\n\nVous avez acheté l'annonce $titre_annonce pour un montant de $montant €. La transaction a été effectuée le $date_transaction.\n\nMerci pour votre achat. N'hésitez pas à prendre contact avec votre acheteur à l'adresse mail suivante : $email_vendeur pour organiser la livraison/récupération de votre bien";
+        $mail->send();
 
         // Envoyer un email au vendeur
-        $sujet_vendeur = "Vente réalisée";
-        $message_vendeur = "Bonjour,\n\nVotre annonce $titre_annonce a été achetée pour un montant de $montant € par l'utilisateur $id_acheteur. La transaction a été effectuée le $date_transaction.\n\nMerci d'organiser la livraison/récupération de l'objet. Vous pouvez le contacter à l'adresse mail suivante : $email_acheteur";
-        mail($email_vendeur, $sujet_vendeur, $message_vendeur, $headers);
+        $mail->setFrom('nitescuale@cy-tech.fr', 'La Bonne Pioche');
+        $mail->addAddress($email_vendeur, 'Nom du destinataire');
+        $mail->isHTML(true);
+        $mail->Subject = 'Bien vendu';
+        $mail->Body = "Bonjour,\n\nVotre annonce $titre_annonce a été achetée pour un montant de $montant € par l'utilisateur $id_acheteur. La transaction a été effectuée le $date_transaction.\n\nMerci d'organiser la livraison/récupération de l'objet. Vous pouvez le contacter à l'adresse mail suivante : $email_acheteur";
+        $mail->send();
 
         echo "Transaction réussie et emails envoyés";
     } else {
